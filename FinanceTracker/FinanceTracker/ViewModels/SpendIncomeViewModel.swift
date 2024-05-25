89e7558cbeb1a777999e23fced110c8756576451
Lev <w9430@yandex.ru>
2024-05-25 15:14:58 +0300

    created spend income view model

---
+++ FinanceTracker/FinanceTracker/ViewModels/SpendIncomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  SpendIncomeViewModel.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 25.05.2024.
+//
+
+import Foundation
+import SwiftUI
+
+final class SpendIncomeViewModel: ObservableObject {
+    //MARK: - Properties
+    private let dataManager: any DataManagerProtocol
+    
+    //MARK: - Initializer
+    init(dataManager: some DataManagerProtocol) {
+        self.dataManager = dataManager
+    }
+    
+    //MARK: - Methods
+}
