a7a800a6f05880126f21dec1223b83faf6cb719a
Lev <w9430@yandex.ru>
2024-05-25 15:14:39 +0300

    created spend income view

---
+++ FinanceTracker/FinanceTracker/Views/SpendIncomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SpendIncomeView.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 25.05.2024.
+//
+
+import SwiftUI
+
+struct SpendIncomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpendIncomeView()
+}
