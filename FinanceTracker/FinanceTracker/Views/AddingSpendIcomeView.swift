6834afeca79f3094a20d2b0fcd5dab1239fd2a88
Lev <w9430@yandex.ru>
2024-05-29 17:07:11 +0300

    SwiftUI View file created

---
+++ FinanceTracker/FinanceTracker/Views/AddingSpendIcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddingSpendIcomeView.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 29.05.2024.
+//
+
+import SwiftUI
+
+struct AddingSpendIcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingSpendIcomeView()
+}
