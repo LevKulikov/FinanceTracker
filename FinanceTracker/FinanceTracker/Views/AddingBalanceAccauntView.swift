1071646c4e6103c531e0ce8265f3b2e56460b8ff
Lev <w9430@yandex.ru>
2024-06-10 14:02:57 +0300

    added View file

---
+++ FinanceTracker/FinanceTracker/Views/AddingBalanceAccauntView.swift
@@ -0,0 +1,28 @@
+//
+//  AddingBalanceAccauntView.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 10.06.2024.
+//
+
+import SwiftUI
+
+struct AddingBalanceAccauntView: View {
+    //MARK: - Properties
+    
+    //MARK: - Initializer
+    
+    //MARK: - Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    //MARK: View Properties
+    
+    //MARK: - Methods
+    
+}
+
+#Preview {
+    AddingBalanceAccauntView()
+}
