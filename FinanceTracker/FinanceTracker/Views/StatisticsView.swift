949a4e6d1f409d565ee4f91eeea63f7bc433010c
Lev <w9430@yandex.ru>
2024-06-13 23:08:58 +0300

    started to build view

--- FinanceTracker/FinanceTracker/Views/StatisticsView.swift
+++ FinanceTracker/FinanceTracker/Views/StatisticsView.swift
@@ -8,15 +8,75 @@
 import SwiftUI
 
 struct StatisticsView: View {
+    //MARK: - Properties
     @StateObject private var viewModel: StatisticsViewModel
     
+    //MARK: - Init
     init(viewModel: StatisticsViewModel) {
         self._viewModel = StateObject(wrappedValue: viewModel)
     }
     
+    //MARK: - Body
     var body: some View {
-        TransactionPieChart(transactions: viewModel.transactions)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    totalValueSection
+
+                    TransactionPieChart(transactionGroups: viewModel.pieChartTransactionData)
+                }
+                .padding()
+            }
+            .refreshable {
+                viewModel.refreshData()
+            }
+            .navigationTitle("Statistics")
+        }
     }
+    
+    //MARK: - Computed View Props
+    private var totalValueSection: some View {
+        VStack {
+            HStack {
+                Text("Total balance")
+                    .font(.title2)
+                    .bold()
+                    .layoutPriority(1)
+                
+                Spacer()
+                
+                Menu(viewModel.balanceAccountToFilter.name) {
+                    Picker("Picker title", selection: $viewModel.balanceAccountToFilter) {
+                        ForEach(viewModel.balanceAccounts) { balanceAccount in
+                            Text(balanceAccount.name)
+                                .tag(balanceAccount)
+                        }
+                    }
+                }
+                .lineLimit(1)
+                .buttonStyle(.bordered)
+            }
+            
+            HStack(alignment: .bottom) {
+                Text(FTFormatters.numberFormatterWithDecimals.string(for: viewModel.totalForBalanceAccount) ?? "Unable to get")
+                    .font(.title)
+                    .underline(color: .secondary)
+                
+                Text(viewModel.balanceAccountToFilter.currency)
+                    .foregroundStyle(.secondary)
+                
+                Spacer()
+            }
+        }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color(.secondarySystemBackground))
+        }
+    }
+    
+    //MARK: - Methods
+    
 }
 
 #Preview {
