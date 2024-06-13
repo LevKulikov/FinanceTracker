23255e963f91b5bcbf1762845fdcdb59d8cb15f2
Lev <w9430@yandex.ru>
2024-06-13 23:09:11 +0300

    updated pie chart view

--- FinanceTracker/FinanceTracker/Views/SupportingViews/TransactionPieChart.swift
+++ FinanceTracker/FinanceTracker/Views/SupportingViews/TransactionPieChart.swift
@@ -7,37 +7,21 @@
 
 import SwiftUI
 import Charts
-import Algorithms
 
 struct TransactionPieChart: View {
-    let transactions: [Transaction]
-    @State private var transactionsGroups: [(categoryName: String, sumValue: Float)] = []
-    
-    init(transactions: [Transaction]) {
-        self.transactions = transactions
-    }
+    let transactionGroups: [(categoryName: String, sumValue: Float)]
     
     var body: some View {
-        Chart(transactionsGroups, id: \.categoryName) { transaction in
+        Chart(transactionGroups, id: \.categoryName) { transaction in
             SectorMark(
                 angle: .value(transaction.categoryName, transaction.sumValue),
                 innerRadius: .ratio(0.6)
             )
             .foregroundStyle(by: .value(Text(verbatim: transaction.categoryName), transaction.categoryName))
         }
-        .onAppear {
-            transactionsGroups = transactions
-                .grouped { $0.category.name }
-                .map { singleDict in
-                    let sumOfValue = singleDict.value.reduce(0, { $0 + $1.value })
-                    let name = singleDict.key
-                    return (categoryName: name, sumValue: sumOfValue)
-                }
-                .sorted(by: { $0.categoryName < $1.categoryName })
-        }
     }
 }
 
 #Preview {
-    TransactionPieChart(transactions: FTAppAssets.testTransactions)
+    TransactionPieChart(transactionGroups: [])
 }
