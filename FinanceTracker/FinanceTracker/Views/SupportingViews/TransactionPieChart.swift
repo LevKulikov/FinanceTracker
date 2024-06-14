3cdb8ea11ce4aa6d1cd015922c4a696cb1257040
Lev <w9430@yandex.ru>
2024-06-15 00:33:53 +0300

    Totally implemented interactive pie chart

--- FinanceTracker/FinanceTracker/Views/SupportingViews/TransactionPieChart.swift
+++ FinanceTracker/FinanceTracker/Views/SupportingViews/TransactionPieChart.swift
@@ -9,52 +9,85 @@ import SwiftUI
 import Charts
 
 struct TransactionPieChart: View {
+    //MARK: - Properties
     let transactionGroups: [(category: Category, sumValue: Float)]
-    private var sumOfValue: Float = 0
     
-    init(transactionGroups: [(category: Category, sumValue: Float)]) {
-        self.transactionGroups = transactionGroups
-        sumOfValue = calculateValue()
+    //MARK: Private pros
+    private var sumOfValue: Float = 0
+    @State private var selectedValue: Int?
+    @State private var selectedCategoryId: String?
+    @State private var cancleDispatchWorkItem: DispatchWorkItem?
+    private var selectedCategoryPersentage: Int? {
+        guard let selectedCategoryId else { return nil }
+        guard let data = transactionGroups.first(where: { $0.category.id == selectedCategoryId }) else { return nil }
+        return calculatePercentage(for: data.sumValue)
+    }
+    private var selectedCategoryValue: Float? {
+        guard let selectedCategoryId else { return nil }
+        guard let data = transactionGroups.first(where: { $0.category.id == selectedCategoryId }) else { return nil }
+        return data.sumValue
     }
     
+    //MARK: - Init
+    init(transactionGroups: [(category: Category, sumValue: Float)]) {
+        self.transactionGroups = transactionGroups
+        sumOfValue = calculateTotalValue()
+    }
+    
+    //MARK: - Body
     var body: some View {
         VStack {
             HStack {
                 Chart {
                     if !transactionGroups.isEmpty {
                         ForEach(transactionGroups, id: \.category) { singleData in
+                            let selectedFlag = isSelected(singleData.category)
+                            
                             SectorMark(
-                                angle: .value(singleData.category.name, singleData.sumValue),
-                                innerRadius: .ratio(0.6)
+                                angle: .value("Sum of transactions", singleData.sumValue),
+                                innerRadius: .ratio(0.65),
+                                outerRadius: .ratio(selectedFlag ? 1 : 0.95),
+                                angularInset: selectedFlag ? 1.5 : 0
                             )
+                            .cornerRadius(selectedFlag ? 3 : 0)
                             .foregroundStyle(singleData.category.color)
+                            .opacity(
+                                selectedCategoryId != nil ? (selectedFlag ? 1 : 0.6) : 1
+                            )
                             .foregroundStyle(by: .value(Text(verbatim: singleData.category.name), singleData.category.name))
                         }
                     } else {
-                        SectorMark(angle: .value("Empy", 100), innerRadius: .ratio(0.6))
-                            .foregroundStyle(Color.gray.opacity(0.5))
+                        SectorMark(
+                            angle: .value("Empy", 100),
+                            innerRadius: .ratio(0.65),
+                            outerRadius: .ratio(0.95)
+                        )
+                        .foregroundStyle(Color.gray.opacity(0.5))
                     }
                 }
+                .chartAngleSelection(value: $selectedValue)
                 .chartLegend(.hidden)
-                .overlay {
-                    Text(FTFormatters.numberFormatterWithDecimals.string(for: sumOfValue) ?? "Err")
-                        .foregroundStyle(.secondary)
-                        .bold()
-                        .frame(width: 110)
-                }
+                .onChange(of: selectedValue, setSelectedCategoryId)
+                .overlay { chartOverlay }
                 
                 ScrollView {
                     VStack(alignment: .leading) {
                         if !transactionGroups.isEmpty {
                             ForEach(transactionGroups, id: \.category) {singleData in
+                                let selectedFlag = isSelected(singleData.category)
+                                
                                 HStack {
                                     BasicChartSymbolShape.circle
                                         .foregroundStyle(singleData.category.color)
                                         .frame(width: 8, height: 8)
                                     
                                     Text(singleData.category.name)
-                                        .foregroundColor(.gray)
+                                        .foregroundColor(selectedFlag ? .primary : .gray)
                                         .font(.caption)
+                                }
+                                .underline(selectedFlag, color: .secondary)
+                                .onTapGesture {
+                                    setSelectedCategory(selectedFlag ? nil : singleData.category)
                                 }
                             }
                         } else {
@@ -76,12 +109,85 @@ struct TransactionPieChart: View {
         }
     }
     
-    private func calculateValue() -> Float {
+    private var chartOverlay: some View {
+        VStack(spacing: 0) {
+            let sumValueDisplay = selectedCategoryId == nil ? sumOfValue : selectedCategoryValue ?? 0
+            
+            Text(FTFormatters.numberFormatterWithDecimals.string(for: sumValueDisplay) ?? "Err")
+                .foregroundStyle(.secondary)
+                .bold()
+            
+            if selectedCategoryId != nil {
+                Divider()
+                    .padding(.horizontal)
+                
+                Text("\(selectedCategoryPersentage ?? 0)%")
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .frame(width: 110)
+    }
+    
+    //MARK: - Methods
+    @ViewBuilder
+    private func getAnnotationPopover(value: Float) -> some View {
+        let percentage = calculatePercentage(for: value)
+        
+        Text("\(percentage)%")
+            .transaction { $0.animation = nil }
+            .padding(.vertical, 5)
+            .padding(.horizontal, 10)
+            .background(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    
+    private func calculateTotalValue() -> Float {
         let value = transactionGroups
             .map { $0.sumValue }
             .reduce(0, +)
         
         return value
+    }
+    
+    private func calculatePercentage(for value: Float) -> Int {
+        return Int(value/sumOfValue * 100)
+    }
+    
+    private func setSelectedCategory(_ category: Category?) {
+        let categoryId = category?.id
+        withAnimation(.snappy(duration: 0.35)) {
+            selectedCategoryId = categoryId
+        } completion: {
+            cancelSelectionAfterDeadline()
+        }
+    }
+    
+    private func setSelectedCategoryId() {
+        guard let selectedValue else { return }
+        var total: Float = 0
+        
+        for element in transactionGroups {
+            total += element.sumValue
+            if Float(selectedValue) <= total {
+                setSelectedCategory(element.category)
+                return
+            }
+        }
+    }
+    
+    private func cancelSelectionAfterDeadline() {
+        guard selectedCategoryId != nil else { return }
+        cancleDispatchWorkItem?.cancel()
+        cancleDispatchWorkItem = DispatchWorkItem {
+            withAnimation {
+                selectedCategoryId = nil
+            }
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: cancleDispatchWorkItem!)
+    }
+    
+    private func isSelected(_ category: Category) -> Bool {
+        return category.id == selectedCategoryId
     }
 }
 
