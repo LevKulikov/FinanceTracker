9d112f0a65b0209d0f89c3df494a92ffabd10941
Lev <w9430@yandex.ru>
2024-06-22 23:30:41 +0300

    First view model configuration

--- FinanceTracker/FinanceTracker/ViewModels/CategoriesViewModel.swift
+++ FinanceTracker/FinanceTracker/ViewModels/CategoriesViewModel.swift
@@ -6,3 +6,64 @@
 //
 
 import Foundation
+import SwiftData
+import SwiftUI
+
+protocol CategoriesViewModelDelegate: AnyObject {
+    func didUpdateCategoryList()
+    
+    func didDeleteCategory()
+}
+
+final class CategoriesViewModel: ObservableObject {
+    //MARK: - Properties
+    weak var delegate: (any CategoriesViewModelDelegate)?
+    
+    //MARK: Private props
+    private var dataManager: any DataManagerProtocol
+    
+    //MARK: Published props
+    @Published private(set) var categories: [Category] = []
+    @Published var caterotyType: TransactionsType = .spending
+    
+    //MARK: - Initializer
+    init(dataManager: some DataManagerProtocol) {
+        self.dataManager = dataManager
+        fetchData()
+    }
+    
+    //MARK: - Methods
+    func fetchData(completionHandler: (() -> Void)? = nil) {
+        Task {
+            await fetchCategories()
+            completionHandler?()
+        }
+    }
+    
+    func getAddingBalanceAccountView(for action: ActionWithCategory) -> some View {
+        return FTFactory.createAddingCategoryView(dataManager: dataManager, transactionType: caterotyType, action: action, delegate: self)
+    }
+    
+    //MARK: Private methods
+    @MainActor
+    private func fetchCategories(errorHandler: (() -> Void)? = nil) {
+        let descriptor = FetchDescriptor<Category>()
+        do {
+            let fetchedData = try dataManager.fetch(descriptor)
+            withAnimation {
+                categories = fetchedData
+            }
+        } catch {
+            errorHandler?()
+        }
+    }
+}
+
+
+//MARK: - Extensions
+//MARK: Extension for AddingCategoryViewModelDelegate
+extension CategoriesViewModel: AddingCategoryViewModelDelegate {
+    func didUpdateCategory() {
+        fetchData()
+    }
+}
