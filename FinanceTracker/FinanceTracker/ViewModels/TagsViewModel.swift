fd25b3f4a9b3bcfccf13c16a2241fd870c55ab90
Lev <w9430@yandex.ru>
2024-06-24 18:14:45 +0300

    added init and delegate protocol

--- FinanceTracker/FinanceTracker/ViewModels/TagsViewModel.swift
+++ FinanceTracker/FinanceTracker/ViewModels/TagsViewModel.swift
@@ -9,14 +9,23 @@ import Foundation
 import SwiftData
 import SwiftUI
 
+protocol TagsViewModelDelegate: AnyObject {
+    
+}
+
 final class TagsViewModel: ObservableObject {
     //MARK: - Properties
+    weak var delegate: (any TagsViewModelDelegate)?
     
     //MARK: Pivate properties
+    private let dataManager: any DataManagerProtocol
     
     //MARK: Published properties
     
     //MARK: - Initializer
+    init(dataManager: some DataManagerProtocol) {
+        self.dataManager = dataManager
+    }
     
     //MARK: - Methods
     
