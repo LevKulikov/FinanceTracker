103fb1a387246c9435a6531584f4d722d49d9e91
Lev <w9430@yandex.ru>
2024-06-24 18:13:00 +0300

    Created view model file

---
+++ FinanceTracker/FinanceTracker/ViewModels/TagsViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  TagsViewModel.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 24.06.2024.
+//
+
+import Foundation
+import SwiftData
+import SwiftUI
+
+final class TagsViewModel: ObservableObject {
+    //MARK: - Properties
+    
+    //MARK: Pivate properties
+    
+    //MARK: Published properties
+    
+    //MARK: - Initializer
+    
+    //MARK: - Methods
+    
+    //MARK: Pivate methods
+}
