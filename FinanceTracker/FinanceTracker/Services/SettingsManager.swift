6dca9fc349522856a1141bf9bc11542893849434
Lev <w9430@yandex.ru>
2024-06-24 22:06:11 +0300

    Settings manager are implemented

---
+++ FinanceTracker/FinanceTracker/Services/SettingsManager.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsManager.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 24.06.2024.
+//
+
+import Foundation
+import SwiftUI
+
+protocol SettingsManagerProtocol: AnyObject {
+    /// Sets tag default color to UserDefaults
+    /// - Parameter color: color to save, nil = delete saved
+    func setTagDefaultColor(_ color: Color?)
+    
+    func getsetTagDefaultColor() -> Color?
+}
+
+final class SettingsManager: SettingsManagerProtocol {
+    //MARK: - Properties
+    private let tagDefaultColorKey = "tagDefaultColorKey"
+    
+    //MARK: - Initializer
+    init() {
+        
+    }
+    
+    //MARK: - Methods
+    func setTagDefaultColor(_ color: Color?) {
+        let uiColor: UIColor? = color == nil ? nil : UIColor(color!)
+        UserDefaults.standard.set(uiColor?.encode(), forKey: tagDefaultColorKey)
+    }
+    
+    func getsetTagDefaultColor() -> Color? {
+        guard let data = UserDefaults.standard.data(forKey: tagDefaultColorKey) else { return nil }
+        guard let uiColor = UIColor.color(data: data) else { return nil }
+        return Color(uiColor: uiColor)
+    }
+}
