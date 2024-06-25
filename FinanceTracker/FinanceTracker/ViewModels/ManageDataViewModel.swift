e37c6ba8064b11c512aadf7d5d92054b652f4485
Lev <w9430@yandex.ru>
2024-06-25 22:30:39 +0300

    view model and delegate protocol configured

--- FinanceTracker/FinanceTracker/ViewModels/ManageDataViewModel.swift
+++ FinanceTracker/FinanceTracker/ViewModels/ManageDataViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+protocol ManageDataViewModelDelegate: AnyObject {
+    func didDeleteAllData()
+}
+
+final class ManageDataViewModel: ObservableObject {
+    //MARK: - Properties
+    weak var delegate: (any ManageDataViewModelDelegate)?
+    
+    //MARK: Private props
+    private let dataManager: any DataManagerProtocol
+    
+    //MARK: - Initializer
+    init(dataManager: some DataManagerProtocol) {
+        self.dataManager = dataManager
+    }
+    
+    //MARK: - Methods
+    func deleteAllStoredData() {
+        
+    }
+}
