c2cf7fad8f12fc70ef5a1b17cc93858cbcf9a9ef
Lev <w9430@yandex.ru>
2024-06-25 21:11:55 +0300

    Configured view model

--- FinanceTracker/FinanceTracker/ViewModels/AppearanceViewModel.swift
+++ FinanceTracker/FinanceTracker/ViewModels/AppearanceViewModel.swift
@@ -6,11 +6,29 @@
 //
 
 import Foundation
+import SwiftUI
 
 final class AppearanceViewModel: ObservableObject {
     //MARK: - Properties
+    private let dataManager: any DataManagerProtocol
+    
+    //MARK: Published props
+    @Published private(set) var preferredColorScheme: ColorScheme? = nil
     
     //MARK: - Initializer
+    init(dataManager: some DataManagerProtocol) {
+        self.dataManager = dataManager
+        self.preferredColorScheme = dataManager.getPreferredColorScheme()
+    }
     
     //MARK: - Methods
+    func setPreferredColorScheme(_ colorScheme: ColorScheme?) {
+        dataManager.setPreferredColorScheme(colorScheme)
+        withAnimation {
+            preferredColorScheme = colorScheme
+        }
+    }
+    
+    //MARK: Private methods
+    
 }
