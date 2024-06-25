e7860bc90841209ecf6450c7725e17db9adf312f
Lev <w9430@yandex.ru>
2024-06-25 20:35:52 +0300

    new methods to settings manager

--- FinanceTracker/FinanceTracker/Services/SettingsManager.swift
+++ FinanceTracker/FinanceTracker/Services/SettingsManager.swift
@@ -14,11 +14,16 @@ protocol SettingsManagerProtocol: AnyObject {
     func setTagDefaultColor(_ color: Color?)
     
     func getTagDefaultColor() -> Color?
+    
+    func setPreferredColorScheme(_ colorScheme: ColorScheme?)
+    
+    func getPreferredColorScheme() -> ColorScheme?
 }
 
 final class SettingsManager: SettingsManagerProtocol {
     //MARK: - Properties
     private let tagDefaultColorKey = "tagDefaultColorKey"
+    private let appColorSchemeUserDefaultsKey = "appColorSchemeUserDefaultsKey"
     
     //MARK: - Initializer
     init() {
@@ -35,5 +40,41 @@ final class SettingsManager: SettingsManagerProtocol {
         guard let data = UserDefaults.standard.data(forKey: tagDefaultColorKey) else { return nil }
         guard let uiColor = UIColor.color(data: data) else { return nil }
         return Color(uiColor: uiColor)
+    }
+    
+    func setPreferredColorScheme(_ colorScheme: ColorScheme?) {
+        /*
+         0 = light
+         1 = dark
+         nil = system color scheme
+         */
+        var colorSchemeIndex: Int?
+        switch colorScheme {
+        case .light:
+            colorSchemeIndex = 0
+        case .dark:
+            colorSchemeIndex = 1
+        case nil:
+            colorSchemeIndex = nil
+        case .some(_):
+            colorSchemeIndex = nil
+        }
+        UserDefaults.standard.setValue(colorSchemeIndex, forKey: appColorSchemeUserDefaultsKey)
+    }
+    
+    func getPreferredColorScheme() -> ColorScheme? {
+        /*
+         0 = light
+         1 = dark
+         nil = system color scheme
+         */
+        guard let colorNumber = UserDefaults.standard.value(forKey: appColorSchemeUserDefaultsKey) as? Int else { return nil }
+        if colorNumber == 0 {
+            return .light
+        } else if colorNumber == 1 {
+            return .dark
+        } else {
+            return nil
+        }
     }
 }
