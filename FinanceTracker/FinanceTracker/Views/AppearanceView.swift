1e40cd0372da003c7f9c29cd59fad4d69e3c3777
Lev <w9430@yandex.ru>
2024-06-25 19:52:23 +0300

    created view file

---
+++ FinanceTracker/FinanceTracker/Views/AppearanceView.swift
@@ -0,0 +1,27 @@
+//
+//  AppearanceView.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 25.06.2024.
+//
+
+import SwiftUI
+
+struct AppearanceView: View {
+    //MARK: - Properties
+    
+    //MARK: - Initializer
+    
+    //MARK: - Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    //MARK: - Computed view props
+    
+    //MARK: - Methods
+}
+
+#Preview {
+    AppearanceView()
+}
