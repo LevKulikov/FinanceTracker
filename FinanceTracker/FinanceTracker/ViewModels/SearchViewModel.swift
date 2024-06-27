3aae5a9fe7c6503ea177fdee305544839acaa3e2
Lev <w9430@yandex.ru>
2024-06-27 14:12:13 +0300

    view model class and protocol

--- FinanceTracker/FinanceTracker/ViewModels/SearchViewModel.swift
+++ FinanceTracker/FinanceTracker/ViewModels/SearchViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol SearchViewModelDelegate: AnyObject {
+    
+}
+
+final class SearchViewModel: ObservableObject {
+    //MARK: - Properties
+    
+    //MARK: - Initializer
+    
+    //MARK: - Methods
+    
+}
