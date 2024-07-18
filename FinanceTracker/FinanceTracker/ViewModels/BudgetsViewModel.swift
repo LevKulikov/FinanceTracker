69b4b16641f16e2b44440ae970149e47445aaa7a
Lev <w9430@yandex.ru>
2024-07-18 19:59:55 +0300

    first view model configuration

--- FinanceTracker/FinanceTracker/ViewModels/BudgetsViewModel.swift
+++ FinanceTracker/FinanceTracker/ViewModels/BudgetsViewModel.swift
@@ -7,10 +7,25 @@
 
 import Foundation
 
+//MARK: - Delegate protocol
 protocol BudgetsViewModelDelegate: AnyObject {
     
 }
 
+//MARK: - ViewModel class
 final class BudgetsViewModel: ObservableObject {
+    //MARK: - Properties
+    weak var delegate: (any BudgetsViewModelDelegate)?
     
+    //MARK: Private properties
+    private let dataManager: any DataManagerProtocol
+    
+    //MARK: - Initializer
+    init(dataManager: any DataManagerProtocol) {
+        self.dataManager = dataManager
+    }
+    
+    //MARK: - Methods
 }
+
+//MARK: - Extensions
