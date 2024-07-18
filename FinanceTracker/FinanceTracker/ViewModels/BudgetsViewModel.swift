53f501949832d7b8e784d2cc30ed506b9dbe8b76
Lev <w9430@yandex.ru>
2024-07-18 20:38:51 +0300

    view model basic configuration

--- FinanceTracker/FinanceTracker/ViewModels/BudgetsViewModel.swift
+++ FinanceTracker/FinanceTracker/ViewModels/BudgetsViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SwiftData
+import SwiftUI
 
 //MARK: - Delegate protocol
 protocol BudgetsViewModelDelegate: AnyObject {
@@ -20,12 +22,71 @@ final class BudgetsViewModel: ObservableObject {
     //MARK: Private properties
     private let dataManager: any DataManagerProtocol
     
+    //MARK: Published props
+    @Published var selectedBalanceAccount: BalanceAccount = .emptyBalanceAccount {
+        didSet {
+            
+        }
+    }
+    @Published private (set) var allBalanceAccounts: [BalanceAccount] = []
+    @Published private(set) var budgets: [Budget] = []
+    
     //MARK: - Initializer
     init(dataManager: any DataManagerProtocol) {
         self.dataManager = dataManager
+        initialFetchData()
     }
     
     //MARK: - Methods
+    func refreshData(compeletionHandler: (() -> Void)? = nil) {
+        Task {
+            await fetchBalanceAccounts()
+            await fetchBudgets()
+            compeletionHandler?()
+        }
+    }
+    
+    //MARK: Private methods
+    private func initialFetchData() {
+        Task { @MainActor in
+            await fetchBalanceAccounts()
+            selectedBalanceAccount = dataManager.getDefaultBalanceAccount() ?? .emptyBalanceAccount
+            await fetchBudgets()
+        }
+    }
+    
+    @MainActor
+    private func fetchBalanceAccounts(errorHandler: ((Error) -> Void)? = nil) async {
+        let descriptor = FetchDescriptor<BalanceAccount>()
+        
+        do {
+            let fetchedBAs = try dataManager.fetch(descriptor)
+            allBalanceAccounts = fetchedBAs
+        } catch {
+            errorHandler?(error)
+        }
+    }
+    
+    @MainActor
+    private func fetchBudgets(errorHandler: ((Error) -> Void)? = nil) async {
+        let copyBalanceAccId = selectedBalanceAccount.persistentModelID
+        
+        let predicate = #Predicate<Budget> {
+            $0.balanceAccount?.persistentModelID == copyBalanceAccId
+        }
+        
+        var descriptor = FetchDescriptor<Budget>(predicate: predicate)
+        descriptor.relationshipKeyPathsForPrefetching = [\.category, \.balanceAccount]
+        
+        do {
+            let fetchedBudgets = try dataManager.fetch(descriptor)
+            withAnimation {
+                budgets = fetchedBudgets
+            }
+        } catch {
+            errorHandler?(error)
+        }
+    }
 }
 
 //MARK: - Extensions
