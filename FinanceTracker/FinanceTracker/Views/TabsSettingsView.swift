3f0b8e2fa265df499c43d2ff61677a5556c225d9
Lev <w9430@yandex.ru>
2024-07-27 20:50:35 +0300

    created view

---
+++ FinanceTracker/FinanceTracker/Views/TabsSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  TabsSettingsView.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 27.07.2024.
+//
+
+import SwiftUI
+
+struct TabsSettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabsSettingsView()
+}
