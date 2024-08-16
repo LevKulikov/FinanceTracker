3c08d1efbeb4596780f7a3f6d5a6fe289d0c4a6e
Lev <w9430@yandex.ru>
2024-08-16 18:22:15 +0300

    added title to provided statistics view

--- FinanceTracker/FinanceTracker/Views/ProvidedStatisticsView.swift
+++ FinanceTracker/FinanceTracker/Views/ProvidedStatisticsView.swift
@@ -28,19 +28,23 @@ struct ProvidedStatisticsView: View {
     
     //MARK: - Body
     var body: some View {
-        ScrollView {
-            VStack {
-                totalsSection
-                
-                pieChartSection
-                
-                barChartSection
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    totalsSection
+                    
+                    pieChartSection
+                    
+                    barChartSection
+                }
+                .padding()
             }
-            .padding()
-        }
-        .scrollIndicators(.hidden)
-        .background {
-            backgroundColor.ignoresSafeArea()
+            .scrollIndicators(.hidden)
+            .navigationTitle("Analytics")
+            .navigationBarTitleDisplayMode(.inline)
+            .background {
+                backgroundColor.ignoresSafeArea()
+            }
         }
     }
     
