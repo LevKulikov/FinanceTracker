2fd9d656a7f3b4bd1f76871550050aff17f551b9
Lev <w9430@yandex.ru>
2024-10-14 22:17:19 +0300

    view file created

---
+++ FinanceTracker/FinanceTracker/Views/AddingTransferView.swift
@@ -0,0 +1,18 @@
+//
+//  AddingTransferView.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 14.10.2024.
+//
+
+import SwiftUI
+
+struct AddingTransferView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingTransferView()
+}
