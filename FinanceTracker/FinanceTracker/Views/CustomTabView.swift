2a3b71ae819812966e9a14d8df3c423de9f41e4f
Lev <w9430@yandex.ru>
2024-11-15 00:18:20 +0300

    optimised tab view for new iPad appearance

--- FinanceTracker/FinanceTracker/Views/CustomTabView.swift
+++ FinanceTracker/FinanceTracker/Views/CustomTabView.swift
@@ -13,6 +13,7 @@ struct CustomTabView: View  {
     @Namespace private var namespace
     @StateObject private var viewModel: CustomTabViewModel
     @State private var actionWithTransaction: ActionWithTransaction = .none
+    @State private var windowSize: CGSize = FTAppAssets.getWindowSize()
     private var availableYOffset: CGFloat {
         if FTAppAssets.currnetUserDeviseName == "iPhone SE (3rd generation)" {
             return 5
@@ -24,6 +25,10 @@ struct CustomTabView: View  {
     }
     private var showAddButton: Bool {
         isSpendIncomeView(tab: viewModel.tabSelection) || viewModel.showAddButtonFromEvetyTab || !viewModel.firstThreeTabs.contains(.spendIncomeView)
+    }
+    private var showNewIpadTabView: Bool {
+        let iosVersion = ProcessInfo.processInfo.operatingSystemVersion.majorVersion
+        return windowSize.width > 660 && iosVersion >= 18 && FTAppAssets.currentUserDevise == .pad
     }
     
     //MARK: - Init
@@ -37,33 +42,57 @@ struct CustomTabView: View  {
             if viewModel.isFirstTabCanBeShown {
                 viewModel.getFirstTab(namespace: namespace)
                     .tag(1)
+                    .tabItem {
+                        labelForTab(1, customLabel: false)
+                    }
             } else {
                 viewModel.getSpendIncomeView(namespace: namespace)
                     .tag(1)
+                    .tabItem {
+                        labelForTab(1, customLabel: false)
+                    }
             }
             
             if viewModel.isSecondTabCanBeShown {
                 viewModel.getSecondTab(namespace: namespace)
                     .tag(2)
+                    .tabItem {
+                        labelForTab(2, customLabel: false)
+                    }
             } else {
                 viewModel.getStatisticsView()
                     .tag(2)
+                    .tabItem {
+                        labelForTab(2, customLabel: false)
+                    }
             }
             
             if viewModel.isThirdTabCanBeShown {
                 viewModel.getThirdTab(namespace: namespace)
                     .tag(3)
+                    .tabItem {
+                        labelForTab(3, customLabel: false)
+                    }
             } else {
                 viewModel.getSearchView()
                     .tag(3)
+                    .tabItem {
+                        labelForTab(3, customLabel: false)
+                    }
             }
             
             if viewModel.isForthTabCanBeShown {
                 viewModel.getForthTab(namespace: namespace)
                     .tag(4)
+                    .tabItem {
+                        labelForTab(4, customLabel: false)
+                    }
             } else {
                 viewModel.getSettingsView()
                     .tag(4)
+                    .tabItem {
+                        labelForTab(4, customLabel: false)
+                    }
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -84,104 +113,125 @@ struct CustomTabView: View  {
         .fullScreenCover(isPresented: $viewModel.isFirstLaunch) {
             viewModel.getWelcomeView()
         }
+        .onGeometryChange(for: CGSize.self) { proxy in
+            proxy.size
+        } action: { newValue in
+            windowSize = newValue
+        }
     }
     
+    @ViewBuilder
     private var customTabView: some View {
-        HStack {
-            let buttonWidth: CGFloat = 70
-            
-            Button {
-                selectTab(1, animated: isSpendIncomeView(tab: 1))
-            } label: {
-                if viewModel.isFirstTabCanBeShown {
-                    viewModel.firstThreeTabs[0].tabLabel
-                } else {
-                    TabViewType.spendIncomeView.tabLabel
+        if showNewIpadTabView {
+            HStack {
+                if showAddButton {
+                    addButton
                 }
             }
-            .frame(width: buttonWidth)
-            .foregroundStyle(viewModel.tabSelection == 1 ? .blue : .secondary)
-            .hoverEffect(.highlight)
-            
-            Spacer()
-            
-            Button {
-                selectTab(2, animated: isSpendIncomeView(tab: 2))
-            } label: {
-                if viewModel.isSecondTabCanBeShown {
-                    viewModel.firstThreeTabs[1].tabLabel
-                } else {
-                    TabViewType.statisticsView.tabLabel
+            .padding(.vertical, 15)
+            .onChange(of: actionWithTransaction) {
+                if case .none = actionWithTransaction {
+                    viewModel.showTabBar = true
                 }
             }
-            .frame(width: buttonWidth)
-            .foregroundStyle(viewModel.tabSelection == 2 ? .blue : .secondary)
-            .hoverEffect(.highlight)
-            
-            if showAddButton {
+        } else {
+            HStack {
+                let buttonWidth: CGFloat = 70
+                
+                Button {
+                    selectTab(1, animated: isSpendIncomeView(tab: 1))
+                } label: {
+                    labelForTab(1)
+                }
+                .frame(width: buttonWidth)
+                .foregroundStyle(viewModel.tabSelection == 1 ? .blue : .secondary)
+                .hoverEffect(.highlight)
+                
                 Spacer()
                 
                 Button {
-                    addButtonTapped()
+                    selectTab(2, animated: isSpendIncomeView(tab: 2))
                 } label: {
-                    ZStack {
-                        Circle()
-                            .fill(.blue)
-                            .matchedGeometryEffect(id: "buttonBackground", in: namespace)
-                            .frame(width: 50)
-                        
-                        Image(systemName: "plus")
-                            .font(.title)
-                            .foregroundStyle(.white)
-                    }
+                    labelForTab(2)
                 }
-                .contentShape([.hoverEffect, .contextMenuPreview], Circle())
+                .frame(width: buttonWidth)
+                .foregroundStyle(viewModel.tabSelection == 2 ? .blue : .secondary)
+                .hoverEffect(.highlight)
+                
+                if showAddButton {
+                    Spacer()
+                    
+                    addButton
+                }
+                
+                Spacer()
+                
+                Button {
+                    selectTab(3, animated: isSpendIncomeView(tab: 3))
+                } label: {
+                    labelForTab(3)
+                }
+                .frame(width: buttonWidth)
+                .foregroundStyle(viewModel.tabSelection == 3 ? .blue : .secondary)
+                .hoverEffect(.highlight)
+                
+                Spacer()
+                
+                Button {
+                    selectTab(4, animated: isSpendIncomeView(tab: 4))
+                } label: {
+                    labelForTab(4)
+                }
+                .frame(width: buttonWidth)
+                .foregroundStyle(viewModel.tabSelection == 4 ? .blue : .secondary)
                 .hoverEffect(.highlight)
             }
-            
-            Spacer()
-            
-            Button {
-                selectTab(3, animated: isSpendIncomeView(tab: 3))
-            } label: {
-                if viewModel.isThirdTabCanBeShown {
-                    viewModel.firstThreeTabs[2].tabLabel
-                } else {
-                    TabViewType.searchView.tabLabel
+            .padding(.horizontal, 5)
+            .padding(.vertical, 15)
+            .frame(maxWidth: 500)
+            .background {
+                Capsule()
+                    .fill(.ultraThinMaterial)
+            }
+            .padding(.horizontal)
+            .onChange(of: actionWithTransaction) {
+                if case .none = actionWithTransaction {
+                    viewModel.showTabBar = true
                 }
             }
-            .frame(width: buttonWidth)
-            .foregroundStyle(viewModel.tabSelection == 3 ? .blue : .secondary)
-            .hoverEffect(.highlight)
-            
-            Spacer()
-            
-            Button {
-                selectTab(4, animated: isSpendIncomeView(tab: 4))
-            } label: {
-                if viewModel.isForthTabCanBeShown {
-                    viewModel.firstThreeTabs[3].tabLabel
-                } else {
-                    TabViewType.settingsView.tabLabel
+        }
+    }
+    
+    private var addButton: some View {
+        Button {
+            addButtonTapped()
+        } label: {
+            if showNewIpadTabView {
+                Label("Add transaction", systemImage: "plus")
+                    .frame(height: 50)
+                    .frame(minWidth: 170)
+                    .padding(.horizontal, 16)
+                    .foregroundStyle(.white)
+                    .background {
+                        Capsule()
+                            .fill(.blue)
+                            .matchedGeometryEffect(id: "buttonBackground", in: namespace)
+                    }
+            } else {
+                ZStack {
+                    Circle()
+                        .fill(.blue)
+                        .matchedGeometryEffect(id: "buttonBackground", in: namespace)
+                        .frame(width: 50)
+                    
+                    Image(systemName: "plus")
+                        .font(.title)
+                        .foregroundStyle(.white)
                 }
             }
-            .frame(width: buttonWidth)
-            .foregroundStyle(viewModel.tabSelection == 4 ? .blue : .secondary)
-            .hoverEffect(.highlight)
         }
-        .padding(.horizontal, 5)
-        .padding(.vertical, 15)
-        .frame(maxWidth: 500)
-        .background {
-            Capsule()
-                .fill(.ultraThinMaterial)
-        }
-        .padding(.horizontal)
-        .onChange(of: actionWithTransaction) {
-            if case .none = actionWithTransaction {
-                viewModel.showTabBar = true
-            }
-        }
+        .contentShape([.hoverEffect, .contextMenuPreview], Circle())
+        .hoverEffect(.highlight)
     }
     
     //MARK: - Methods
@@ -211,6 +261,70 @@ struct CustomTabView: View  {
     private func isSpendIncomeView(tab: Int) -> Bool {
         guard viewModel.firstThreeTabs.count > (tab - 1) else { return false }
         return viewModel.firstThreeTabs[tab - 1] == .spendIncomeView
+    }
+    
+    @ViewBuilder
+    private func labelForTab(_ tabNumber: Int, customLabel: Bool = true) -> some View {
+        switch tabNumber {
+        case 1:
+            if viewModel.isFirstTabCanBeShown {
+                if customLabel {
+                    viewModel.firstThreeTabs[0].tabLabel
+                } else {
+                    viewModel.firstThreeTabs[0].label
+                }
+            } else {
+                if customLabel {
+                    TabViewType.spendIncomeView.tabLabel
+                } else {
+                    TabViewType.spendIncomeView.label
+                }
+            }
+        case 2:
+            if viewModel.isSecondTabCanBeShown {
+                if customLabel {
+                    viewModel.firstThreeTabs[1].tabLabel
+                } else {
+                    viewModel.firstThreeTabs[1].label
+                }
+            } else {
+                if customLabel {
+                    TabViewType.statisticsView.tabLabel
+                } else {
+                    TabViewType.statisticsView.label
+                }
+            }
+        case 3:
+            if viewModel.isThirdTabCanBeShown {
+                if customLabel {
+                    viewModel.firstThreeTabs[2].tabLabel
+                } else {
+                    viewModel.firstThreeTabs[2].label
+                }
+            } else {
+                if customLabel {
+                    TabViewType.searchView.tabLabel
+                } else {
+                    TabViewType.searchView.label
+                }
+            }
+        case 4:
+            if viewModel.isForthTabCanBeShown {
+                if customLabel {
+                    viewModel.firstThreeTabs[3].tabLabel
+                } else {
+                    viewModel.firstThreeTabs[3].label
+                }
+            } else {
+                if customLabel {
+                    TabViewType.settingsView.tabLabel
+                } else {
+                    TabViewType.settingsView.label
+                }
+            }
+        default:
+            EmptyView()
+        }
     }
 }
 
