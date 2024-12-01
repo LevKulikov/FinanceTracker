b6975c0f2fcd1df71e076aae3ebd9e88142ee31d
Lev <w9430@yandex.ru>
2024-12-02 02:23:56 +0300

    Added view to add or update search config

---
+++ FinanceTracker/FinanceTracker/Views/AddUpdateSearchConfigurationView.swift
@@ -0,0 +1,18 @@
+//
+//  AddUpdateSearchConfigurationView.swift
+//  FinanceTracker
+//
+//  Created by Лев Куликов on 02.12.2024.
+//
+
+import SwiftUI
+
+struct AddUpdateSearchConfigurationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddUpdateSearchConfigurationView()
+}
