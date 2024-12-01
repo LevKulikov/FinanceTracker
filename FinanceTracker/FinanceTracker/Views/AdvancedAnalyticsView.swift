99c26214b3b3a67a5e9a3a938e32bf417f9e13b1
Lev <w9430@yandex.ru>
2024-12-02 00:55:40 +0300

    configured view initialiser

--- FinanceTracker/FinanceTracker/Views/AdvancedAnalyticsView.swift
+++ FinanceTracker/FinanceTracker/Views/AdvancedAnalyticsView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct AdvancedAnalyticsView: View {
+    //MARK: - Properties
+    @StateObject private var viewModel: AdvancedAnalyticsViewModel
+    
+    //MARK: - Initializer
+    init(viewModel: AdvancedAnalyticsViewModel) {
+        self._viewModel = .init(wrappedValue: viewModel)
+    }
+
+    //MARK: - Computed View Properties
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+    
+    //MARK: - Methods AdvancedAnalyticsViewModel
 }
 
 #Preview {
-    AdvancedAnalyticsView()
+    let dataManager = DataManager(container: FinanceTrackerApp.createModelContainer())
+    let viewModel = AdvancedAnalyticsViewModel(dataManager: dataManager)
+    AdvancedAnalyticsView(viewModel: viewModel)
 }
