11194fadfb6e372fd151fe19a2b15af40f3a44a6
Lev <w9430@yandex.ru>
2024-12-02 02:25:10 +0300

    the first configuration of major view

--- FinanceTracker/FinanceTracker/Views/AdvancedAnalyticsView.swift
+++ FinanceTracker/FinanceTracker/Views/AdvancedAnalyticsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct AdvancedAnalyticsView: View {
     //MARK: - Properties
     @StateObject private var viewModel: AdvancedAnalyticsViewModel
+    @State private var showAddingFilterView: Bool = false
+    @State private var updatedConfiguration: SearchConfiguration?
     
     //MARK: - Initializer
     init(viewModel: AdvancedAnalyticsViewModel) {
@@ -18,10 +20,72 @@ struct AdvancedAnalyticsView: View {
 
     //MARK: - Computed View Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                if viewModel.searchConfigurations.isEmpty {
+                    noFilterAddedView
+                }
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 130))]) {
+                    ForEach(viewModel.searchConfigurations) { config in
+                        ConfigurationCell(config)
+                    }
+                }
+                .padding(.horizontal, 10)
+            }
+            .navigationTitle("Advanced Analytics")
+            .toolbar {
+                Button("Add filter", systemImage: "plus") {
+                    showAddingFilterView.toggle()
+                }
+                .labelStyle(.iconOnly)
+            }
+            .sheet(isPresented: $showAddingFilterView) {
+                AddUpdateSearchConfigurationView(configuration: nil, balanceAccounts: viewModel.allBalanceAccounts, categories: viewModel.allCategories, tags: viewModel.allTags) { config in
+                    viewModel.addConfiguration(config)
+                    showAddingFilterView.toggle()
+                }
+            }
+            .sheet(item: $updatedConfiguration) { config in
+                AddUpdateSearchConfigurationView(configuration: config, balanceAccounts: viewModel.allBalanceAccounts, categories: viewModel.allCategories, tags: viewModel.allTags) { configuration in
+                    viewModel.updateConfiguration(configuration)
+                    updatedConfiguration = nil
+                }
+            }
+        }
     }
     
-    //MARK: - Methods AdvancedAnalyticsViewModel
+    private var noFilterAddedView: some View {
+        ContentUnavailableView {
+            Label("No filters added", systemImage: "tray")
+        } description: {
+            Text("Add any number of filters to search for transactions of interest and get analytics on them. Analytics will reflect unique transactions")
+        } actions: {
+            Button("Add filter") {
+                showAddingFilterView.toggle()
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+    
+    //MARK: - Methods
+    @ViewBuilder
+    private func ConfigurationCell(_ configuration: SearchConfiguration) -> some View {
+        VStack {
+            Text(configuration.filterTransactionType.rawValue)
+            Text(configuration.dateFilterType.rawValue)
+            Text(configuration.filterBalanceAccount?.name ?? "All accounts")
+            Text(configuration.filterCategory?.name ?? "All categories")
+        }
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color.secondary.opacity(0.5))
+        }
+        .onTapGesture {
+            updatedConfiguration = configuration
+        }
+    }
 }
 
 #Preview {
