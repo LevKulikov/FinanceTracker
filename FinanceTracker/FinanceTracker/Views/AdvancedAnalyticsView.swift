d1ad67b981a1600012a7505e87061c57a19d19bf
Lev <w9430@yandex.ru>
2024-12-02 23:28:38 +0300

    the next configuration of major view

--- FinanceTracker/FinanceTracker/Views/AdvancedAnalyticsView.swift
+++ FinanceTracker/FinanceTracker/Views/AdvancedAnalyticsView.swift
@@ -13,6 +13,8 @@ struct AdvancedAnalyticsView: View {
     @Environment(\.colorScheme) private var colorScheme
     @State private var showAddingFilterView: Bool = false
     @State private var updatedConfiguration: SearchConfiguration?
+    @State private var windowSize: CGSize = FTAppAssets.getWindowSize()
+    @State private var removeAllAlert: Bool = false
     private let userDevice = FTAppAssets.currentUserDevise
     private var isIpad: Bool {
         FTAppAssets.currentUserDevise == .pad
@@ -31,7 +33,7 @@ struct AdvancedAnalyticsView: View {
                     noFilterAddedView
                 }
                 
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 130))]) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: (windowSize.width / 2) - 20))]) {
                     ForEach(viewModel.searchConfigurations) { config in
                         ConfigurationCell(config)
                     }
@@ -48,6 +50,13 @@ struct AdvancedAnalyticsView: View {
                 if viewModel.isLoadingOtherData {
                     ProgressView()
                 } else {
+                    if !viewModel.searchConfigurations.isEmpty {
+                        Button("Delete all filters", systemImage: "trash") {
+                            removeAllAlert.toggle()
+                        }
+                        .labelStyle(.iconOnly)
+                    }
+                    
                     Button("Add filter", systemImage: "plus") {
                         showAddingFilterView.toggle()
                     }
@@ -84,6 +93,22 @@ struct AdvancedAnalyticsView: View {
             .sheet(isPresented: $viewModel.showAnalytics) {
                 viewModel.getAnalyticsPage()
             }
+            .alert("No transactions found", isPresented: $viewModel.noTransactions) {
+                Button("Ok") {}
+            } message: {
+                Text("Try to change filters to get more transactions")
+            }
+            .alert("Remove all filters?", isPresented: $removeAllAlert) {
+                Button("No", role: .cancel) {}
+                Button("Yes", role: .destructive) {
+                    viewModel.removeAllConfigurations()
+                }
+            }
+            .onGeometryChange(for: CGSize.self) { proxy in
+                proxy.size
+            } action: { newValue in
+                windowSize = newValue
+            }
         }
     }
     
@@ -104,7 +129,7 @@ struct AdvancedAnalyticsView: View {
         Button {
             viewModel.getAnalytics()
         } label: {
-            Label("Get analytics", systemImage: "chart.bar")
+            Label("Get analytics", systemImage: "chart.line.text.clipboard")
                 .frame(width: 170, height: 50)
                 .background {
                     Capsule()
@@ -112,6 +137,7 @@ struct AdvancedAnalyticsView: View {
                         .stroke(.blue)
                 }
         }
+        .hoverEffect(.lift)
         .offset(y: -5)
     }
     
@@ -136,6 +162,7 @@ struct AdvancedAnalyticsView: View {
                 .fill(colorScheme == .light ? Color(.systemBackground) : Color(.systemGray6))
         }
         .padding()
+        .shadow(radius: 10)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background {
             Rectangle()
@@ -148,19 +175,71 @@ struct AdvancedAnalyticsView: View {
     //MARK: - Methods
     @ViewBuilder
     private func ConfigurationCell(_ configuration: SearchConfiguration) -> some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text(getDateText(configuration))
+                .foregroundStyle(.secondary)
+            
             Text(configuration.filterTransactionType.rawValue)
-            Text(configuration.dateFilterType.rawValue)
+                .foregroundStyle(configuration.filterTransactionType.color)
+            
             Text(configuration.filterBalanceAccount?.name ?? "All accounts")
+                .foregroundStyle(configuration.filterBalanceAccount?.color ?? .primary)
+            
             Text(configuration.filterCategory?.name ?? "All categories")
+                .foregroundStyle(configuration.filterCategory?.color ?? .primary)
+            
+            if configuration.filterTags.isEmpty {
+                Text("0 tags")
+                    .foregroundStyle(.secondary)
+            } else {
+                let firstTag = configuration.filterTags.first!
+                Text("\(firstTag.name)")
+                    .foregroundStyle(firstTag.color)
+                +
+                Text(configuration.filterTags.count > 1 ? " + \(configuration.filterTags.count - 1)" : "")
+                    .foregroundStyle(.secondary)
+            }
         }
+        .lineLimit(1)
+        .font(.footnote)
         .padding()
         .background {
             RoundedRectangle(cornerRadius: 12)
-                .fill(Color.secondary.opacity(0.5))
+                .fill(Color(.systemGray5))
+                .stroke(.secondary, style: .init(lineWidth: 1))
         }
         .onTapGesture {
             updatedConfiguration = configuration
+        }
+        .contentShape([.hoverEffect, .contextMenuPreview], RoundedRectangle(cornerRadius: 12.0))
+        .contextMenu {
+            Button("Delete", systemImage: "trash", role: .destructive) {
+                viewModel.removeConfiguration(configuration)
+            }
+            
+            Button("Duplicate", systemImage: "document.on.document") {
+                viewModel.duplicateConfiguration(configuration)
+            }
+        }
+    }
+    
+    private func getDateText(_ configuration: SearchConfiguration) -> String {
+        let date = configuration.filterDate
+        let dateComponents = Calendar.current.dateComponents([.weekOfMonth, .month, .year], from: date)
+        
+        switch configuration.dateFilterType {
+        case .day:
+            return date.formatted(date: .abbreviated, time: .omitted)
+        case .week:
+            return "\(String(localized: "Week")) \(dateComponents.weekOfMonth ?? 0), \(date.month) \(dateComponents.year ?? 0)"
+        case .month:
+            return "\(date.month) \(dateComponents.year ?? 0)"
+        case .year:
+            return "\(dateComponents.year ?? 0)"
+        case .customDateRange:
+            let startDateString = configuration.filterDateStart.formatted(date: .abbreviated, time: .omitted)
+            let endDateString = configuration.filterDateEnd.formatted(date: .abbreviated, time: .omitted)
+            return "\(startDateString) - \(endDateString)"
         }
     }
 }
